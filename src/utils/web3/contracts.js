30234359d4a6d03a260c9f1c3a075590c65021ec
YunJeongJoo <yjj4899@aniverse.io>
2022-09-28 11:12:08 +0900

    컨트렉트 추가

--- src/utils/web3/contracts.js
+++ src/utils/web3/contracts.js
@@ -1,15 +1,15 @@
 const contracts = {
     current_nft_contract: {
         1001: "0x6a0320be3b86ec319462703c9b9bfd109c33a8c9",  // klaytn testnet
-        8217: "",  // klaytn mainnet
+        8217: "0x03546f472b320883a368f3247b349597eee4be28",  // klaytn mainnet
     },
     reveal_contract: {
         1001: "0x54f93aa4b8140ff706cf14b8762c356b8142ad3c",  // klaytn testnet
-        8217: "",  // klaytn mainnet
+        8217: "0x570ba898554344fce3e50fae0161a95a6fccc1eb",  // klaytn mainnet
     },
     reveal_nft_contract: {
         1001: "0x051ccd21cf8f60926e071904c6e164497a7ef472",  // klaytn testnet
-        8217: "",  // klaytn mainnet
+        8217: "0xc0ed656ff9050a2a79681fc3f4ba9e816b1e9a98",  // klaytn mainnet
     },
 };
 
